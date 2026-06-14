753da5935aed362b5fc5a60429d628160398e4ff
fabomat3000-spec <fabomat3000@gmail.com>
2026-06-14 13:47:22 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Managing SnapGo and working on updates!",
+        name: "Managing SnapGo!",
         // Activity type number (0 = Playing).
         type: 3, 
       },
